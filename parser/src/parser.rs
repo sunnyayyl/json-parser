f5caa4b514aa0c651aba8a8983fa52e39dbb78e9
sunny.ayyl <sunny.ayyl@gmail.com>
2025-03-13 10:07:44 +0000

    Array and properly parse non-literal value

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -1,4 +1,3 @@
-
 use crate::cursor::TokenCursor;
 use lexer::{Lexer, LexerToken, LiteralType};
 
@@ -8,6 +7,7 @@ pub enum Value {
     String(String),
     Number(f64),
     Bool(bool),
+    Array(Array),
     Null,
 }
 impl From<LiteralType> for Value {
@@ -35,6 +35,15 @@ pub struct Members(Vec<Member>);
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Object(Option<Members>);
+#[derive(Debug, PartialEq, Clone)]
+
+pub struct Elements(Vec<Element>);
+#[derive(Debug, PartialEq, Clone)]
+
+pub struct Element(Value);
+#[derive(Debug, PartialEq, Clone)]
+
+pub struct Array(Option<Elements>);
 pub struct Parser<'a> {
     cursor: TokenCursor<'a>,
 }
@@ -47,20 +56,55 @@ impl<'a> Parser<'a> {
     pub fn parse(&mut self) -> Object {
         self.parse_object().expect("Empty JSON?")
     }
+    #[inline]
+    fn parse_element(&mut self) -> Element {
+        Element(self.parse_value())
+    }
+    fn parse_elements(&mut self) -> Elements {
+        let mut elemets = vec![];
+        elemets.push(self.parse_element());
+        while self.cursor.peek() == Some(LexerToken::Comma) {
+            self.cursor.next_token();
+            elemets.push(self.parse_element());
+        }
+        Elements(elemets)
+    }
+    fn parse_array(&mut self) -> Array {
+        self.expect_next(LexerToken::LeftBracket);
+        let elements = self.parse_elements();
+        self.expect_next(LexerToken::RightBracket);
+        Array(Some(elements))
+    }
+    fn parse_value(&mut self) -> Value {
+        let next = self.cursor.peek();
+        match next {
+            Some(LexerToken::Literal(literal)) => {
+                self.cursor.next_token();
+                literal.into()
+            }
+            Some(LexerToken::LeftBrace) => Value::Object(
+                self.parse_object()
+                    .expect("Expected to find an object, got None"),
+            ),
+            Some(LexerToken::LeftBracket) => Value::Array(self.parse_array()),
+            Some(_) => panic!("Unexpected token {:?}", next),
+            None => panic!("Value expected"),
+        }
+    }
     fn parse_member(&mut self) -> Member {
         let key;
-        let value;
         if let Some(LexerToken::Literal(LiteralType::String(literal))) = self.cursor.next_token() {
             key = literal;
         } else {
             panic!("Expected string literal as key")
         }
         self.expect_next(LexerToken::Colon);
-        if let Some(LexerToken::Literal(literal)) = self.cursor.next_token() {
+        /*if let Some(LexerToken::Literal(literal)) = self.cursor.next_token() {
             value = literal;
         } else {
             panic!("Expected literal as value")
-        }
+        }*/
+        let value = self.parse_value();
         Member::new(key, value.into())
     }
     fn parse_members(&mut self) -> Members {
@@ -96,13 +140,25 @@ mod tests {
     use super::*;
     #[test]
     fn test_ast() {
-        let mut parser = Parser::new(Lexer::new("{\"key\":\"value\", \"key2\\\"\":2}"));
+        let mut parser = Parser::new(Lexer::new(
+            "{\"key\":\"value\", \"key2\\\"\":[1,\"test\", {\"testing\": \"ok\"}]}",
+        ));
         let obj = parser.parse_object();
         assert_eq!(
             obj,
             Some(Object(Some(Members(vec![
                 Member::new("key".to_string(), Value::String("value".to_string())),
-                Member::new("key2\"".to_string(), Value::Number(2.0))
+                Member::new(
+                    "key2\"".to_string(),
+                    Value::Array(Array(Some(Elements(vec![
+                        Element(Value::Number(1.0)),
+                        Element(Value::String("test".to_string())),
+                        Element(Value::Object(Object(Some(Members(vec![Member::new(
+                            "testing".to_string(),
+                            Value::String("ok".to_string())
+                        )])))))
+                    ]))))
+                )
             ]))))
         );
     }
